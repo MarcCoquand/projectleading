bad863ab9fa38e3227d51479ae4a0565a2e561a9
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2016-12-06 22:47:17 +0100

    Implemented prototype

---
+++ src/res.js
@@ -0,0 +1,30 @@
+var strings1 = [
+    "Some string 1",
+    "Some string 2",
+    "Some string 3",
+    "A lot of strings",
+    "Wieieieie so many strings",
+    "Ugh, som many stirngs",
+    "I don't even",
+    "Okay, hold on! This is going to be such a long string! Whoa, look at me! I write the longest strings, u can't even understand how long this string is. It can even have BANGS in it, look: BANG, BANG! Cool huh?"
+]
+
+var strings2 = [
+    "Some string from 2"
+]
+
+var strings3 = [
+    "Some string from kjdfkjdfkjdfkj"
+]
+
+var strings = {
+    0: strings1,
+    1: strings2,
+    2: strings3
+}
+
+var images = [
+    "route1.png",
+    "route2.png",
+    "route3.png"
+]
